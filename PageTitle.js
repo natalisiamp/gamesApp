1043f6caa24a478378b701325bffd5f00f17cf86
natalisiam <101431623+natalisiamp@users.noreply.github.com>
2022-07-07 13:11:43 -0400

    Add files via upload

---
+++ PageTitle.js
@@ -0,0 +1,9 @@
+
+import React from 'react';
+function PageTitle()//this has to match the name of the file
+{
+   return(
+     <h1 id="title">This si the title of the page</h1>
+   );
+};
+export default PageTitle;
